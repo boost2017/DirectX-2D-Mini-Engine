b46c591593f0c26c3c90a9d80c35a80f650667a2
Cyril Garsaud <cyril.garsaud@gmail.com>
2015-10-08 02:07:19 -0500

    Major memory fixes

--- Prefabs/SpritePrefab.cs
+++ Prefabs/SpritePrefab.cs
@@ -8,9 +8,16 @@ namespace SlimDX.Common
 	public class SpritePrefab
 	{
 		#region Public Interface
-		public System.Drawing.Bitmap tileSheetBitmap;
 		public Rectangle tileRect;
 		public Point drawToPosition;
+		public System.Drawing.Bitmap tileSheetBitmap
+		{
+			get { return this._tileSheetBitmap; }
+			set {
+				this._d2dbitmap = null;
+				this._tileSheetBitmap = value;
+			}
+		}
 
 		public SpritePrefab(DeviceContext2D deviceContext2D)
 		{
@@ -19,37 +26,40 @@ namespace SlimDX.Common
 		
 		public void Draw()
 		{
-			// Load the texture
-			var bitmapData = this.tileSheetBitmap.LockBits(
-				tileRect,
-				System.Drawing.Imaging.ImageLockMode.ReadOnly,
-				System.Drawing.Imaging.PixelFormat.Format32bppPArgb
-			);
-			var dataStream = new DataStream(
-				bitmapData.Scan0,
-				bitmapData.Stride * bitmapData.Height,
-				true,
-				false
-			);
-			var d2dPixelFormat = new PixelFormat(
-				SlimDX.DXGI.Format.B8G8R8A8_UNorm,
-				SlimDX.Direct2D.AlphaMode.Premultiplied
-			);
-			var d2dBitmapProperties = new BitmapProperties();
-			d2dBitmapProperties.PixelFormat = d2dPixelFormat;
+			if (this._d2dbitmap == null)
+			{
+				// Load the texture
+				var bitmapData = this.tileSheetBitmap.LockBits(
+					tileRect,
+					System.Drawing.Imaging.ImageLockMode.ReadOnly,
+					System.Drawing.Imaging.PixelFormat.Format32bppPArgb
+				);
+				var dataStream = new DataStream(
+					bitmapData.Scan0,
+					bitmapData.Stride * bitmapData.Height,
+					true,
+					false
+				);
+				var d2dPixelFormat = new PixelFormat(
+					SlimDX.DXGI.Format.B8G8R8A8_UNorm,
+					SlimDX.Direct2D.AlphaMode.Premultiplied
+				);
+				var d2dBitmapProperties = new BitmapProperties();
+				d2dBitmapProperties.PixelFormat = d2dPixelFormat;
 
-			var d2dBitmap = new SlimDX.Direct2D.Bitmap(
-				_device.RenderTarget,
-				tileRect.Size,
-				dataStream,
-				bitmapData.Stride,
-				d2dBitmapProperties
-			);
-			this.tileSheetBitmap.UnlockBits(bitmapData);
+				_d2dbitmap = new SlimDX.Direct2D.Bitmap(
+					_device.RenderTarget,
+					tileRect.Size,
+					dataStream,
+					bitmapData.Stride,
+					d2dBitmapProperties
+				);
+				this.tileSheetBitmap.UnlockBits(bitmapData);
+			}
 
 			// Render to the 2D context
 			_device.RenderTarget.DrawBitmap(
-				d2dBitmap,
+				_d2dbitmap,
 				new Rectangle(
 					this.drawToPosition,
 					new Size(this.tileRect.Width, this.tileRect.Height)
@@ -60,6 +70,8 @@ namespace SlimDX.Common
 
 		#region Implementation Details
 		protected DeviceContext2D _device;
+		protected SlimDX.Direct2D.Bitmap _d2dbitmap;
+		private System.Drawing.Bitmap _tileSheetBitmap;
 		#endregion
 	}
 }
